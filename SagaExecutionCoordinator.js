65dccfb01ddb4a03e7baff0d872d1d164588c2ce
Jay <jay@ismotgroup.com>
2018-06-03 11:54:53 +0430

    fix merge values

--- SagaExecutionCoordinator.js
+++ SagaExecutionCoordinator.js
@@ -73,9 +73,9 @@ class SagaExecutionCoordinator {
     for (const tcId in flow) {
       const tc = flow[tcId];
       if (tc.transaction && tc.transaction.value) {
-        mergedTransactionValues = _.merge(mergedTransactionValues, tc.transaction.value);
+        mergedTransactionValues = _.asign(mergedTransactionValues, tc.transaction.value);
       } else if (tc.compensation && tc.compensation.value) {
-        mergedCompensationValues = _.merge(mergedCompensationValues, tc.compensation.value);
+        mergedCompensationValues = _.asign(mergedCompensationValues, tc.compensation.value);
       }
     }
 
